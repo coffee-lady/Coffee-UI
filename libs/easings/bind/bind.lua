c6a65f6dc6cf690edb56c3c2406b7a8ea95525c0
Anastasiya <sima.leitch@gmail.com>
2022-03-05 15:33:18 +0300

    Initial Commit

---
+++ libs/easings/bind/bind.lua
@@ -0,0 +1,6 @@
+local Bind = {
+    Go = require('libs.tools.easings.bind.go_bind'),
+    Gui = require('libs.tools.easings.bind.gui_bind')
+}
+
+return Bind
---
+++ libs/math/geometry/geometry.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+M.PathGenerator = require('libs.tools.math.geometry.path_generator')
+M.Bezier = require('libs.tools.math.geometry.bezier')
+
+return M
